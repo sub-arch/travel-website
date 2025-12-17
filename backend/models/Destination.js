f30ca92e16c98bc391b5b53742704444396e706f
subhra <SubhraPradhan.bca.2023@gmail.com>
2025-12-17 15:01:42 +0530

    Create Destination.js

---
+++ backend/models/Destination.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const destinationSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Destination', destinationSchema);
+
+
